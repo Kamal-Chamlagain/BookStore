f735568bea2622807d8c17a7a40f53194198ac7d
KamalChamlagain <kamalchamlagain15@gmail.com>
2022-09-15 23:23:36 +0545

    Third Commit

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,11 +1,14 @@
-﻿namespace BookStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
 {
     public class Category
     {
-
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
