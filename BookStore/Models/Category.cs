a2ee1adb2cf6c5338c98f450d38d29082e6fa76e
KamalChamlagain <kamalchamlagain15@gmail.com>
2022-09-15 22:52:42 +0545

    model created

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -2,5 +2,11 @@
 {
     public class Category
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
